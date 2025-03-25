bb7f135e6e27f286e77d225962ed25779b2653de
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2025-03-25 14:01:59 +0800

    update dep
    Signed-off-by: Asutorufa <16442314+Asutorufa@users.noreply.github.com>

--- app/docs/connections/failed/page.tsx
+++ app/docs/connections/failed/page.tsx
@@ -7,6 +7,7 @@ import { Button, Spinner, Table } from "react-bootstrap"
 import styles from "../../common/clickable.module.css"
 import Loading from "../../common/loading"
 import { useProtoSWR } from "../../common/proto"
+import { type } from "../../pbes/statistic/config_pb"
 import { connections, failed_history } from "../../pbes/statistic/grpc/config_pb"
 
 const TimestampZero = create(TimestampSchema, { seconds: BigInt(0), nanos: 0 })
@@ -56,7 +57,7 @@ function FailedHistory() {
                         return (
                             <tr key={"bh-" + index}>
                                 <td>{timestampDate(v.time!).toLocaleString()}</td>
-                                <td>{v.protocol}</td>
+                                <td>{type[v.protocol ?? type.unknown]}</td>
                                 <td>{v.host}</td>
                                 <td>{Number(v.failedCount)}</td>
                                 <td>{v.error}</td>
