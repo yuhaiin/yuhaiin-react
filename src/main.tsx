bc0909711704857fa0fd0b0b96e4880926ec770b
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2026-01-25 01:46:10 +0800

    fix typo
    Signed-off-by: Asutorufa <16442314+Asutorufa@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { RouterProvider, createHashHistory, createRouter } from '@tanstack/react-router';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './global.css';
 import { routeTree } from './routeTree.gen';
 
+const hashHistory = createHashHistory()
+
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree, history: hashHistory })
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
